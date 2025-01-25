774e66346028d36e73da0942365d7fab7ca7a6fe
0lexy-the-cutie0 <aallen360isawsome@gmail.com>
2025-01-24 20:45:48 -0600

    update run script to add the ability to quick flash boards

--- run.sh
+++ run.sh
@@ -1,8 +1,21 @@
-(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
-	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
-        && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
-        && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
-	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
-	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-	&& sudo apt update \
-	&& sudo apt install gh -y
+#!/bin/bash
+
+# . ./.venv/bin/activate  # Uncomment if using a virtual environment
+
+list1=$(ls /dev/tty*)
+echo "Press enter to continue after you have plugged in the board while holding the SW1 button to place it into update mode"
+read
+
+sleep 6s
+
+list2=$(sudo ls /dev/tty*)
+flash_port=$(comm -13 <(echo "$list1" | sort) <(echo "$list2" | sort))
+
+if [ -z "$flash_port" ]; then
+    echo "No new device detected. Exiting."
+    exit 1
+fi
+
+echo "Detected new device on $flash_port"
+# Flash the firmware
+python3 -m tools.ectf25.utils.flash ./decoder/build_out/max78000.bin $flash_port
