f3cee0ed5de1c6f10927706a6497fdf18ea7ee51
0lexy-the-cutie0 <aallen360isawsome@gmail.com>
2025-01-24 21:56:03 -0600

    updated flash.sh script so that it automatically builds the enviroment

--- flash.sh
+++ flash.sh
@@ -8,7 +8,7 @@ read
 
 sleep 6s
 
-list2=$(sudo ls /dev/tty*)
+list2=$(ls /dev/tty*)
 flash_port=$(comm -13 <(echo "$list1" | sort) <(echo "$list2" | sort))
 
 if [ -z "$flash_port" ]; then
@@ -17,5 +17,14 @@ if [ -z "$flash_port" ]; then
 fi
 
 echo "Detected new device on $flash_port"
+sleep 4s
+#Enter virtual enviroment for building and flashing firmware
+. ./.venv/bin/activate
+#Build the firmware using docker
+docker build -t decoder ./decoder
+docker run --rm -v ./decoder/build_out:/out -v ./decoder/:/decoder -v ./secrets:/serets -e DECODER_ID=0xdeadbeef decoder
 # Flash the firmware
 python3 -m tools.ectf25.utils.flash ./decoder/build_out/max78000.bin $flash_port
+# Exit virtual enviroment so that shell is usable again
+deactivate
+echo "Successfully flashed to board"
